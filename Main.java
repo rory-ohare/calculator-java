2d155f39e241ba960a053b5a5fbd7164f5d66e68
Rory O'Hare <roryeoinohare@gmail.com>
2025-02-11 00:11:30 +0000

    added some constructors and basic methods

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ import utils.Calculator;
 class Main{
     public static void main(String[] args){
         Calculator calc = new Calculator();
-        calc.print();
+
+        calc.add(5);
+        System.out.println(calc.getCurrentValue());
+        calc.sub(2);
+        System.out.println(calc.getCurrentValue());
     }
 }
--- utils/Calculator.java
+++ utils/Calculator.java
@@ -1,6 +1,39 @@
 package utils;
 
 public class Calculator{
+    private int currentValue;
+
+    // Default Constructor
+    public Calculator(){
+        this.currentValue = 0;
+    }
+
+    // General Constructor
+    public Calculator(int initial){
+        this.currentValue = initial;
+    }
+
+    public int getCurrentValue(){
+        return currentValue;
+    }
+
+    // Operations
+    public void add(int val){
+        this.currentValue += val;
+    }
+
+    public void sub(int val){
+        this.currentValue -= val;
+    }
+
+    public void reset(){
+        this.currentValue = 0;
+    }
+
+    public void reset(int val){
+        this.currentValue = val;
+    }
+
     public void print(){
         System.out.println("Hello world");
     }
